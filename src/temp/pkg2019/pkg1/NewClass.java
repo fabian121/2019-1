54e463213f7f616ecd982ebc196a7c82c7419982
fabian <fabian121@gmail.com>
2019-04-02 07:40:36 -0600

    new new

---
+++ src/temp/pkg2019/pkg1/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package temp.pkg2019.pkg1;
+
+/**
+ *
+ * @author root
+ */
+public class NewClass {
+    private String name;
+}
